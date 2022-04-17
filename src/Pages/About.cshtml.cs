4b13719ac11864d75bfcda02b501e10c54b21e32
SrimithraBingi <bingisrimithra@gmail.com>
2022-04-17 14:52:30 -0700

    Adding New Pages to Website
    - Added new pages: ContactUs and About in the solution to fix broken links and also to suit site design.
    
    -Updated Layout.cshtml so that there are no broken links or errors.

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    public class PrivacyModel : PageModel
+    public class AboutModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        private readonly ILogger<AboutModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
+        public AboutModel(ILogger<AboutModel> logger) => _logger = logger;
 
         public void OnGet()
         {
---
+++ src/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        private readonly ILogger<ContactUsModel> _logger;
+
+        public ContactUsModel(ILogger<ContactUsModel> logger) => _logger = logger;
+        public void OnGet()
+        {
+        }
+    }
+}
+
