2288eb33ec9f7ee414e368c3e36fe60188a965c7
J.H <59863142+JoenHo@users.noreply.github.com>
2022-04-23 15:36:31 -0700

    Created Privacy page as Privacy.cshtml and Privacy.cshtml.cs

---
+++ src/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// This is class for the privacy page
+    /// </summary>
+    public class PrivacyModel : PageModel
+    {
+        /// <summary>
+        /// This function is called when the privacy page is called 
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
+
