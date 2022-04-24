c553967d55d7006c9819e6f2e866c93b0fa47023
troy8chen <troy8chen@gmail.com>
2022-04-23 17:53:24 -0700

    Add inline comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,11 +3,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+
     public class Program
     {
+        /// <summary>
+        /// This is the function for applications to start, serves as
+        /// enter point of the application
+        /// </summary>
+        /// <param name="args"></param> take strings array to mark start point
         public static void Main(string[] args) =>
+            // This method creates new instance of HostBuilder
             CreateHostBuilder(args).Build().Run();
 
+        // This method calls UserStartup<start>() method from
+        // webBuilder to specify the Startu class to be accessed by web host
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
