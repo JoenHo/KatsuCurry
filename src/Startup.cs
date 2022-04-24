f0484fa312af5ba5c41a701c5dc6c97e282a566b
troy8chen <troy8chen@gmail.com>
2022-04-23 18:01:01 -0700

    Added inline comment for Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -9,14 +9,18 @@ namespace ContosoCrafts.WebSite
 {
     public class Startup
     {
+        // This method is to initiate the configuration method
         public Startup(IConfiguration configuration) =>
             Configuration = configuration;
 
+        // This method is to call configuration object
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// This method gets called by the runtime. Use this method to add
+        /// services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding all the services we need
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddHttpClient();
@@ -24,7 +28,8 @@ namespace ContosoCrafts.WebSite
             services.AddTransient<JsonFileProductService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// This method gets called by the runtime. Use this method to
+        /// configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -34,10 +39,14 @@ namespace ContosoCrafts.WebSite
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change
+                // this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            // Following serveice is to configuate the network and
+            // make sure the end points are reachable
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -51,10 +60,13 @@ namespace ContosoCrafts.WebSite
                 endpoints.MapControllers();
                 endpoints.MapBlazorHub();
 
+                // Might be using for later
                 // endpoints.MapGet("/products", (context) => 
                 // {
-                //     var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
-                //     var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                //     var products = app.ApplicationServices.GetService
+                //     <JsonFileProductService>().GetProducts();
+                //     var json = JsonSerializer.Serialize
+                //     <IEnumerable<Product>>(products);
                 //     return context.Response.WriteAsync(json);
                 // });
             });
