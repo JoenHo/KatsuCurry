f172ce5c5361ccdb0eeff6ad61b4ffa716fc9f5e
SrimithraBingi <bingisrimithra@gmail.com>
2022-04-25 14:24:08 -0700

    Added comments to the model Food.cs so that it is better documented.

--- src/Models/Food.cs
+++ src/Models/Food.cs
@@ -3,13 +3,22 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// This class holds C# representation of the Food.json data
+    /// </summary>
     public class Food
     {
+
+        // followings are the properties of Food data
         public string? restaurant { get; set; }
         public string? description { get; set; }
         [JsonPropertyName("img")]
         public string? image { get; set; }
 
+        /// <summary>
+        /// This method converts the class objects back to JSON representation
+        /// </summary>
+        /// Returns a string of JSON representation of the class objects</returns>
         public override string ToString() => JsonSerializer.Serialize<Food>(this);
     }
 }
