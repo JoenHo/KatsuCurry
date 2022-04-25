ef28300e5a4326f5c1f99e24f3e6d37bdacada33
SrimithraBingi <bingisrimithra@gmail.com>
2022-04-25 14:22:20 -0700

    Added a model Food.cs for the food data.

---
+++ src/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Food
+    {
+        public string? restaurant { get; set; }
+        public string? description { get; set; }
+        [JsonPropertyName("img")]
+        public string? image { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Food>(this);
+    }
+}
