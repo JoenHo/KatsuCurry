9c8667787610205273a7e9a5518e80fc4e420a47
Joen Ho <59863142+JoenHo@users.noreply.github.com>
2022-04-25 11:45:21 -0700

    Added constructor to PrivacyModel class in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
@@ -12,6 +13,16 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class PrivacyModel : PageModel
     {
+
+        // logger object
+        private readonly ILogger<PrivacyModel> _logger;
+
+        /// <summary>
+        /// Constructor for PrivacyModel class
+        /// </summary>
+        /// <param name="logger">logger object</param>
+        public PrivacyModel(ILogger<PrivacyModel> logger) => _logger = logger;
+
         /// <summary>
         /// Whenever a user makes a GET request to a page, this method is invoked
         /// </summary>
