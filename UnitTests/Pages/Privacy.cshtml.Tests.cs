c63ab52d87893d8f4fdd1869504b0102c8495beb
Joen Ho <59863142+JoenHo@users.noreply.github.com>
2022-04-25 11:55:22 -0700

    Created unit tests for PrivacyModel class as Privacy.cshtml.Tests.cs
    - adapted professor's code along with more comments
    - code coverage 100%

---
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Pages.Privacy
+{
+    /// <summary>
+    /// This class is for unit testing PrivacyModel class
+    /// The tests cover the following methods:
+    /// - OnGet()
+    /// </summary>
+    public class PrivacyTests
+    {
+        #region TestSetup
+        public static PrivacyModel pageModel;
+
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // initialize PrivacyModel instance with ILogger
+            var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
+            pageModel = new PrivacyModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// ModelState.IsValid should return true after OnGet is called
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Model_State_Return_IsValisd_True()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
+    }
+}
