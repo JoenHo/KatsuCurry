dc27fcd4e021990d3e9aae48675fd51285dac24c
Joen Ho <59863142+JoenHo@users.noreply.github.com>
2022-04-25 11:50:03 -0700

    Added initial setup for unit testing as TestFixture.cs, TestHelper.cs, StartupTests.cs, and UnitTests.csproj
    - adapted professor's code along with more comments

---
+++ UnitTests/StartupTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+
+using NUnit.Framework;
+
+namespace UnitTests.Pages.Startup
+{
+    /// <summary>
+    /// This class is to tests the Startup.cs
+    /// /// The tests cover the following methods:
+    /// - ConfigureServices()
+    /// - Configure()
+    /// </summary>
+    public class StartupTests
+    {
+        #region TestSetup
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        public class Startup : ContosoCrafts.WebSite.Startup
+        {
+            public Startup(IConfiguration config) : base(config) { }
+        }
+        #endregion TestSetup
+
+        #region ConfigureServices
+        /// <summary>
+        /// Creating webHost via Startup, webHost should not be null
+        /// </summary>
+        [Test]
+        public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
+        {
+            var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            Assert.IsNotNull(webHost);
+        }
+        #endregion ConfigureServices
+
+        #region Configure
+        /// <summary>
+        /// Creating webHost via Startup, webHost should not be null
+        /// </summary>
+        [Test]
+        public void Startup_Configure_Valid_Defaut_Should_Pass()
+        {
+            var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            Assert.IsNotNull(webHost);
+        }
+        #endregion Configure
+    }
+}
---
+++ UnitTests/TestFixture.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// This class provides a fixed state of a set of objects used as a baseline for running tests
+    /// </summary>
+    [SetUpFixture]
+    public class TestFixture
+    {
+        // Path to the Web Root
+        public static string DataWebRootPath = "./wwwroot";
+
+        // Path to the data folder for the content
+        public static string DataContentRootPath = "./data/";
+
+        /// <summary>
+        /// This method runs once per test harness execution 
+        /// to ensure unit test project to always reference the latest datasets
+        /// </summary>
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            // Run this code once when the test harness starts up.
+
+            // This will copy over the latest version of the database files
+
+            var DataWebPath = "../../../../src/bin/Debug/net5.0/wwwroot/data";
+            var DataUTDirectory = "wwwroot";
+            var DataUTPath = DataUTDirectory + "/data";
+
+            // Delete the Detination folder
+            if (Directory.Exists(DataUTDirectory))
+            {
+                Directory.Delete(DataUTDirectory, true);
+            }
+            
+            // Make the directory
+            Directory.CreateDirectory(DataUTPath);
+
+            // Copy over all data files
+            var filePaths = Directory.GetFiles(DataWebPath);
+            foreach (var filename in filePaths)
+            {
+                string OriginalFilePathName = filename.ToString();
+                var newFilePathName = OriginalFilePathName.Replace(DataWebPath, DataUTPath);
+
+                File.Copy(OriginalFilePathName, newFilePathName);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+        }
+    }
+}
