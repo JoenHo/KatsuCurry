b4d025862c15635776da0833cfda1d48a184431c
SrimithraBingi <bingisrimithra@gmail.com>
2022-04-29 16:20:56 -0700

    Added a default Constructor in ProductIndex.cshtml

--- src/Pages/Product/ProductIndex.cshtml.cs
+++ src/Pages/Product/ProductIndex.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,16 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class ProductIndexModel : PageModel
     {
-        public void OnGet()
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public ProductIndexModel(JsonFileProductService productService)
         {
+            ProductService = productService;
         }
+
+        
     }
 }
 
