ca16478afbd49368cdc7d9311a16f3227fc0f6d4
Owen Clinton <owenwjc@gmail.com>
2022-04-30 22:58:45 -0700

    Added Delete Data method to JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -154,5 +154,22 @@ namespace ContosoCrafts.WebSite.Services
 
             return productData;
         }
+
+        /// <summary>
+        /// Remove the item from the system
+        /// </summary>
+        /// <returns></returns>
+        public ContosoCrafts.WebSite.Models.Product DeleteData(string id)
+        {
+            // Get the current set, and append the new record to it
+            var dataSet = GetProducts();
+            var data = dataSet.FirstOrDefault(m => m.Id.Equals(id));
+
+            var newDataSet = GetProducts().Where(m => m.Id.Equals(id) == false);
+            
+            SaveData(newDataSet);
+
+            return data;
+        }
     }
 }
