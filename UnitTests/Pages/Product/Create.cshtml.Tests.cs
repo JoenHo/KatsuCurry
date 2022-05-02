04129ededef7c037d453b908f3d2a28f7d993c68
Troy Chen(he/him/his) <troy8chen@gmail.com>
2022-05-02 13:57:11 -0700

    Update Create.cshtml.Tests.cs
    Set up unit test environment in Create.cshtml.Tests.cs

--- UnitTests/Pages/Product/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ContosoCrafts.WebSite.Pages.Product;
+using NUnit.Framework;
 
-namespace UnitTests.Pages.Product
+namespace UnitTests.Pages.Product.Create
 {
-    class Create
+    class CreateTest
     {
+        #region TestSetup
+        public static CreateModel pageModel;
+
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new CreateModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+
     }
 }
