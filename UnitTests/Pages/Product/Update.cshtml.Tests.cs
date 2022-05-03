33edf99e27f31de2ec9e129132d2d9b185b0ef66
Owen Clinton <owenwjc@gmail.com>
2022-05-02 21:25:05 -0700

    Added tutorial unit testing code to Update.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+
+namespace UnitTests.Pages.Product.Update
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new UpdateModel(TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("selinazawacki-shirt");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Floppy Crop", pageModel.Product.Title);
+        }
+        #endregion OnGet
+
+        #region OnPostAsync
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "selinazawacki-moon",
+                Title = "title",
+                Description = "description",
+                Url = "url",
+                Image = "image"
+            };
+
+            // Act
+            var result = pageModel.OnPost() as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
+        }
+
+        [Test]
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            pageModel.Product = new ProductModel
+            {
+                Id = "bogus",
+                Title = "bogus",
+                Description = "bogus",
+                Url = "bogus",
+                Image = "bougs"
+            };
+
+            // Force an invalid error state
+            pageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            // Act
+            var result = pageModel.OnPost() as ActionResult;
+
+            // Assert
+            Assert.AreEqual(false, pageModel.ModelState.IsValid);
+        }
+        #endregion OnPostAsync
+    }
+}
