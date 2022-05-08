e5f0bbc43bcc93463d79144f4eef4111b467427b
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-07 18:42:27 -0700

    Added comments in UpdateFood.cshtml.cs

--- src/Pages/Product/UpdateFood.cshtml.cs
+++ src/Pages/Product/UpdateFood.cshtml.cs
@@ -8,15 +8,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// This class defines the pgae model of UpdateFood page 
+    /// </summary>
     public class UpdateFoodModel : PageModel
     {
+        // service for acuiring data
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="productService"></param>
         public UpdateFoodModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        /// <summary>
+        /// This function is called when GET requested
+        /// </summary>
         public void OnGet()
         {
         }
