e919ceef6f7df46f234a461cde5fecc0787e11ad
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-07 17:51:44 -0700

    Created a page model from template for creating food data as CreateFood.cshtml.cs

---
+++ src/Pages/Product/CreateFood.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class CreateFoodModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+
