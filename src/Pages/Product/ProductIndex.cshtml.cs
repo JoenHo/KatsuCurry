3dc041fedcad8b09a065e380a4d134e57bad6788
troy8chen <troy8chen@gmail.com>
2022-05-17 13:08:21 -0700

    Remove non-using statement inProductIndex.cshtml.cs

--- src/Pages/Product/ProductIndex.cshtml.cs
+++ src/Pages/Product/ProductIndex.cshtml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ContosoCrafts.WebSite.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Product
