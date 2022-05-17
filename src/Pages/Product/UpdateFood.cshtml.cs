4443869b085fffa83467087d12a01a02ae0814a9
troy8chen <troy8chen@gmail.com>
2022-05-17 13:10:38 -0700

    Remove non-using statement in UpdateFood.cshtml.cs

--- src/Pages/Product/UpdateFood.cshtml.cs
+++ src/Pages/Product/UpdateFood.cshtml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
