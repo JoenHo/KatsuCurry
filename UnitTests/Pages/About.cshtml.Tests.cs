6df00e62a2bc941ff341d3030d8428ff0f7026de
Owen Clinton <owenwjc@gmail.com>
2022-05-17 18:33:24 -0700

    Added tests to About.cshtml.Tests.cs

---
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -0,0 +1,56 @@
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.About
+{
+
+    /// <summary>
+    /// This class is for unit testing IndexModel class
+    /// The tests cover the following methods:
+    /// - OnGet()
+    /// </summary>
+    public class AboutTests
+    {
+        #region TestSetup
+
+        public static AboutModel pageModel;
+
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
+
+            pageModel = new AboutModel(MockLoggerDirect)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// ModelState.IsValid should return true after OnGet is called
+        /// With valid activity, Products should contain items
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
+    }
+}
