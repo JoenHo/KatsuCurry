fa836358b11fe715431a593282254f7a69577e61
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-18 18:41:26 -0700

    Dereferenced possible null references in Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -29,7 +29,7 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// <param name="id">item id</param>
         public void OnGet(string id)
         {
-            var checknull = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+            var checknull = ProductService.GetProducts().FirstOrDefault(m => m.Id != null && m.Id.Equals(id));
             if (checknull is not null) {
                 restaurants = checknull;
             }
