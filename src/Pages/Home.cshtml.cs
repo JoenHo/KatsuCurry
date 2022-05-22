bf2f29676572a0b36a89ce08d5f61e4a80be7b9a
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-22 11:31:37 -0700

    Migrated entire contents from Index .cshtml.cs to Home.cshtml.cs

---
+++ src/Pages/Home.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public class IndexModel : PageModel
+        {
+            private readonly ILogger<IndexModel> _logger;
+
+            public IndexModel(ILogger<IndexModel> logger,
+                JsonFileProductService productService)
+            {
+                _logger = logger;
+                ProductService = productService;
+            }
+
+            public JsonFileProductService ProductService { get; }
+            public IEnumerable<Models.Product>? Products { get; private set; }
+
+            public void OnGet() => Products = ProductService.GetProducts();
+        }
+    }
+}
