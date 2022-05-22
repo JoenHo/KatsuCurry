660830c3fa8ba6490c38698780e127428ba0bb02
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-22 11:32:37 -0700

    Added inline comments in Home.cshtml.cs

--- src/Pages/Home.cshtml.cs
+++ src/Pages/Home.cshtml.cs
@@ -5,12 +5,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Apply simple and efficient authorize attribute for razor page
+    /// </summary>
     public class HomeModel : PageModel
     {
+        /// <summary>
+        /// Apply simple and efficient authorize attribute for razor page
+        /// </summary>
         public class IndexModel : PageModel
         {
+            // THe following methods are simple implementation of
+            // C# System under HTTPClient to call a request
+            // to our desire API, logger and ProdocutService
+            // then post it and display to homepage for viewing
             private readonly ILogger<IndexModel> _logger;
 
+            /// <summary>
+            /// Constructor for IndexModel class
+            /// </summary>
+            /// <param name="logger">logger object</param>
             public IndexModel(ILogger<IndexModel> logger,
                 JsonFileProductService productService)
             {
@@ -21,6 +35,9 @@ namespace ContosoCrafts.WebSite.Pages
             public JsonFileProductService ProductService { get; }
             public IEnumerable<Models.Product>? Products { get; private set; }
 
+            /// <summary>
+            /// Whenever a user makes a GET request to a page, this method is invoked
+            /// </summary>
             public void OnGet() => Products = ProductService.GetProducts();
         }
     }
