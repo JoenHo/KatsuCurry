09ced37dd5f44682096de93999587c7013baef60
Owen Clinton <owenwjc@gmail.com>
2022-05-24 16:36:44 -0700

    Simplified class structure for home page Home.cshtml.cs

--- src/Pages/Home.cshtml.cs
+++ src/Pages/Home.cshtml.cs
@@ -10,35 +10,29 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class HomeModel : PageModel
     {
+        // THe following methods are simple implementation of
+        // C# System under HTTPClient to call a request
+        // to our desire API, logger and ProdocutService
+        // then post it and display to homepage for viewing
+        private readonly ILogger<HomeModel> _logger;
+
         /// <summary>
-        /// Apply simple and efficient authorize attribute for razor page
+        /// Constructor for HomeModel class
         /// </summary>
-        public class IndexModel : PageModel
+        /// <param name="logger">logger object</param>
+        public HomeModel(ILogger<HomeModel> logger,
+            JsonFileProductService productService)
         {
-            // THe following methods are simple implementation of
-            // C# System under HTTPClient to call a request
-            // to our desire API, logger and ProdocutService
-            // then post it and display to homepage for viewing
-            private readonly ILogger<IndexModel> _logger;
-
-            /// <summary>
-            /// Constructor for IndexModel class
-            /// </summary>
-            /// <param name="logger">logger object</param>
-            public IndexModel(ILogger<IndexModel> logger,
-                JsonFileProductService productService)
-            {
-                _logger = logger;
-                ProductService = productService;
-            }
-
-            public JsonFileProductService ProductService { get; }
-            public IEnumerable<Models.Product>? Products { get; private set; }
-
-            /// <summary>
-            /// Whenever a user makes a GET request to a page, this method is invoked
-            /// </summary>
-            public void OnGet() => Products = ProductService.GetProducts();
+            _logger = logger;
+            ProductService = productService;
         }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Models.Product>? Products { get; private set; }
+
+        /// <summary>
+        /// Whenever a user makes a GET request to a page, this method is invoked
+        /// </summary>
+        public void OnGet() => Products = ProductService.GetProducts();
     }
 }
