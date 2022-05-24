0de406cc4e9c4706d01b87ebff0f8bddae73cc4f
Owen Clinton <owenwjc@gmail.com>
2022-05-24 16:37:20 -0700

    Added test cases for Home page Home.cshtml.Tests.cs

---
+++ UnitTests/Pages/Home.cshtml.Tests.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Home
+{
+
+    /// <summary>
+    /// This class is for unit testing HomeModel class
+    /// The tests cover the following methods:
+    /// - OnGet()
+    /// </summary>
+    public class HomeTests
+    {
+        #region TestSetup
+
+        public static HomeModel pageModel;
+
+        /// <summary>
+        /// Setting up the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<HomeModel>>();
+
+            pageModel = new HomeModel(MockLoggerDirect, TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        /// <summary>
+        /// ModelState.IsValid should return true after OnGet is called
+        /// With valid activity, Products should contain items
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Return_Page()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
+    }
+}
