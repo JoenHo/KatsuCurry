b826a6b07fdae9e5fb8cbd0e4c24e5a8bb980cc7
Owen Clinton <owenwjc@gmail.com>
2022-05-30 21:24:15 -0700

    Made new response model for google api call response Response.cs

---
+++ src/Models/Response.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// This class holds C# representation of the Response.json data
+    /// </summary>
+    public class Response
+    {
+
+        // followings are the properties of Google Places API response
+        public Candidates[] candidates { get; set; }
+
+        public string? status { get; set; }
+
+        /// <summary>
+        /// This method converts the class objects back to JSON representation
+        /// </summary>
+        /// Returns a string of JSON representation of the class objects</returns>
+        public override string ToString() => JsonSerializer.Serialize<Response>(this);
+    }
+
+    public class Candidates
+    {
+        public string formatted_address { get; set; }
+        public string name { get; set; }
+        public string place_id { get; set; }
+    }
+}
