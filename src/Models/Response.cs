67e779e231e42102f802415cb500e813cc9ad8ee
Owen Clinton <owenwjc@gmail.com>
2022-05-31 20:23:11 -0700

    Removed tostring method from response model Response.cs

--- src/Models/Response.cs
+++ src/Models/Response.cs
@@ -14,12 +14,7 @@ namespace ContosoCrafts.WebSite.Models
         public Candidates[]? candidates { get; set; }
 
         public string? status { get; set; }
-
-        /// <summary>
-        /// This method converts the class objects back to JSON representation
-        /// </summary>
-        /// Returns a string of JSON representation of the class objects</returns>
-        public override string ToString() => JsonSerializer.Serialize<Response>(this);
+        
     }
 
     public class Candidates
