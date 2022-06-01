270588fd8ad70e069327108e64f497d3a80cb6d7
troy8chen <troy8chen@gmail.com>
2022-05-31 22:02:24 -0700

    Added exception handling with code 400 & 404 in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -34,6 +34,16 @@ namespace ContosoCrafts.WebSite
         {
             if (env.IsDevelopment())
             {
+                app.Use(async (context, next) =>
+                {
+                    await next();
+                    if (context.Response.StatusCode == 404 || context.Response.StatusCode == 400)
+                    {
+                        context.Request.Path = "/Error";
+                        await next();
+                    }
+                });
+
                 app.UseExceptionHandler("/Error");
             }
             else
